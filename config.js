02231429bd013aa2311ebcd3b09452ce161e598b
puneethtejas19-eng <puneeth.tejas19@gmail.com>
2026-02-13 12:21:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dplfabo0m/video/upload/v1771003192/Lord_Huron_-_The_Night_We_Met_Lyrics_rvjpah.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
